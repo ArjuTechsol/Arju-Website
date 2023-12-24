e87d9337f7c16f6c94774915b8280959cb36c52c
ArjuTechsol <arjutechsol1@gmail.com>
2023-12-24 17:41:20 +0530

    contact form changes

--- script.js
+++ script.js
@@ -1,57 +1,41 @@
 
 ////////////////////////////////// CONTACT FORM CODE//////////////////////////////////
 
-//This event is fired when the initial HTML document has been completely loaded and parsed.
-// document.addEventListener('DOMContentLoaded', () => {
-//   //It then retrieves references to the contact form and a container element where the server response will be displayed.
-//   const contactForm = document.getElementById('contactForm');
-//   const responseContainer = document.getElementById('responseContainer');
+document.addEventListener('DOMContentLoaded', () => {
+  const contactForm = document.getElementById('contactForm');
+  const responseContainer = document.getElementById('responseContainer');
 
-//   contactForm.addEventListener('submit', async (event) => {
-//     // When the form is submitted, it prevents the default form submission behavior.
-//     event.preventDefault();
+  contactForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-//     // It creates a new FormData object from the contact form, then converts it to a plain JavaScript object.
-//     const formData = new FormData(contactForm);
-//     const formDataObject = {};
-//     formData.forEach((value, key) => {
-//       formDataObject[key] = value;
-//     });
+    const formData = new FormData(contactForm);
+    const formDataObject = {};
+    formData.forEach((value, key) => {
+      formDataObject[key] = value;
+    });
 
-//     try {
-//       //The code then uses the Fetch API to send a POST request to the server with the form data as JSON.
-//       const response = await fetch('/api/contact', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(formDataObject),
-//       });
+    try {
+      // Simulating a successful submission without making an actual request
+      // In a real scenario, you would replace this with your actual form submission logic.
+      // For now, we'll just use a setTimeout to simulate an asynchronous operation.
+      setTimeout(() => {
+        // Clear the form fields if needed
+        contactForm.reset();
 
-//       //Handling Server Response
-//       const data = await response.json();
-//       responseContainer.textContent = `Server Response: ${JSON.stringify(data)}`;
+        // Display a success message
+        responseContainer.textContent = 'Form submitted successfully!';
 
-//       // Error Handling
-//     } catch (error) {
-//       console.error('Error submitting form:', error);
-//       responseContainer.textContent = 'Error submitting form. Please try again.';
-//     }
-//   });
-// });
-
-////////////////////////////////// CONTACT FORM NEW CODE//////////////////////////////////
-
-// let form = document.getElementById('contactForm');
-
-// form.addEvenetListner('submit', function(e){
-//   e.preventDefault();
-  
-//   let formData = new FormData(e.target);
-//   let formObj = Object.fromEntries(formData);
-//   console.log(formObj)
-// });
-
+        // Clear the message after 5 seconds
+        setTimeout(() => {
+          responseContainer.textContent = '';
+        }, 5000); // 5000 milliseconds = 5 seconds
+      }, 1000); // Simulating a delay of 1 second (adjust as needed)
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      responseContainer.textContent = 'Error submitting form. Please try again.';
+    }
+  });
+});
 
 ////////////////////////////////// BACK TO TOP BUTTON CODE//////////////////////////////////
 
@@ -65,7 +49,7 @@ document.addEventListener("DOMContentLoaded", () => {
   mybutton.style.display = "none";
 
   // When the user scrolls down 100px from the top of the document, show the button
-  window.onscroll = function() {scrollFunction()};
+  window.onscroll = function () { scrollFunction() };
   function scrollFunction() {
 
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
@@ -86,29 +70,41 @@ document.addEventListener("DOMContentLoaded", () => {
 
 ////////////////////////////////// REVIEW PAGE CODE//////////////////////////////////
 
-// Set current index to 0
+// Set initial index to 0
 let currentIndex = 0;
 
-// When th user clicks on nex button, this function gets called
+// Function to move the slide based on the given offset
+function moveSlide(offset) {
+  // Update currentIndex using modulo to ensure it stays within the range [0, 6]
+  currentIndex = (currentIndex + offset + 7) % 7;
+  // Call the function to update the slider UI
+  updateSlider();
+}
+
+// Function to handle the next slide
 function nextSlide() {
-    currentIndex = (currentIndex + 1) % 10; // Adjust the number based on the number of cards
-    updateSlider();
+  // Call moveSlide with an offset of 1 to move to the next slide
+  moveSlide(1);
 }
 
-// When the user clicks on the prev button, this function gets called
+// Function to handle the previous slide
 function prevSlide() {
-    currentIndex = (currentIndex - 1 + 10) % 10; // Adjust the number based on the number of cards
-    updateSlider();
+  // Call moveSlide with an offset of -1 to move to the previous slide
+  moveSlide(-1);
 }
 
-// Above both functions calls this function to update the slider
+// Function to update the slider UI
 function updateSlider() {
-    const sliderContent = document.querySelector(".slider-content");
-    const cardWidth = document.querySelector(".review-card").offsetWidth + 20; // Adjusted width including margin
-        
-    sliderContent.style.transform = `translateX(${-currentIndex * cardWidth }px)`;
-       
+  // Get the slider content element
+  const sliderContent = document.querySelector(".slider-content");
+  // Get the width of a review card including margin
+  const cardWidth = document.querySelector(".review-card").offsetWidth + 20;
+
+  // Update the transform property to move the slider to the current index
+  sliderContent.style.transform = `translateX(${-currentIndex * cardWidth}px)`;
 }
+
+
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
