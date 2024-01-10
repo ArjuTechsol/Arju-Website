9da2786af50e831ed3190a10ea6fbc1db4347ce6
ArjuTechsol <arjutechsol1@gmail.com>
2024-01-10 16:09:35 +0530

    GSAP Scroll Trigger 2

--- javascripts/gsap2.js
+++ javascripts/gsap2.js
@@ -1,113 +1,168 @@
+//////////////////  GSAP ANIMATION FOR LANDING PAGE //////////////////
+
+var tl = gsap.timeline();
 
 
-    
-    // const navItems = document.querySelectorAll(".nav-items");
-    
-    // var tl = gsap.timeline();
+tl.from(".logo ", {
+    opacity: 0,
+    scale: 0,
+    duration: 0.5
+})
 
-    // navItems.forEach((item, index) => {
-    //     // Initial state
-    //     tl.from(item, {
-    //         opacity: 0,
-    //         y: -40,
-    //         yoyo: true
-    //     }, index * 0.1);
-    // });
+tl.from(".nav-bar ", {
+    opacity: 0,
+    duration: 0.5
+})
 
-    gsap.from(".home ", {
-        y: -40,
-    })
+tl.from(".home", {
+    y: -50,
+    duration: 0.15
+})
 
+tl.from(".about ", {
+    y: -50,
+    duration: 0.15
+})
 
-    gsap.from(".quote-wrapper", {
-        scale: 0.5,
-        opacity: 0,
-        scrollTrigger:{
-            trigger:".quote-wrapper",
-            scroller:"body",
-            start: "top 70%",
-            end: "top 40%",
-            scrub:2,
-        }
-    })
+tl.from(".serv ", {
+    y: -50,
+    duration: 0.15
+})
 
-    
+tl.from(".rev ", {
+    y: -50,
+    duration: 0.15
+})
 
-    //////////////////  GSAP ANIMATION FOR ABOUT PAGE //////////////////
-    
-    gsap.from(".image1", {
-        x: -900,
-        duration: 1,
-        scrollTrigger:{
-            trigger:".image1",
-            scroller:"body",
-            start: "top 70%",
-            end: "top 40%",
-            scrub:2,
-        }
-    })
-    
-    gsap.from(".image2", {
-        y: 200,
-        duration: 1,
-        scrollTrigger:{
-            trigger:".image2",
-            scroller:"body",
-            start: "top 70%",
-            end: "top 40%",
-            scrub:1,
-        }
-    })
-    
-    gsap.from(".image3", {
-        x: 900,
-        duration: 1,
-        scrollTrigger:{
-            trigger:".image3",
-            scroller:"body",
-            start: "top 70%",
-            end: "top 40%",
-            scrub:2,
-        }
-    })
+tl.from(".con ", {
+    y: -50,
+    duration: 0.15
+})
 
 
-    gsap.from(".box h2", {
-        y: -50,
-        opacity: 0,
-        stagger: 0.4,
-        scrollTrigger:{
-            trigger:".box h2",
-            scroller:"body",
-            start: "top 70%",
-            end: "top 65%",
-            scrub:1,
-        }
-    })
-    
-    gsap.from(".box p", {
-        x: -400,
-        opacity: 0,
-        stagger: 0.4,
-        scrollTrigger:{
-            trigger:".box p",
-            scroller:"body",
-            start: "top 70%",
-            end: "top 65%",
-            scrub:1,
-        }
-    })
-    
-    gsap.from(".see-more span", {
-        opacity: 0 ,
-        stagger: 0.4,
-        scrollTrigger:{
-            trigger:".see-more span",
-            scroller:"body",
-            start: "top 90%",
-            end: "top 80%",
-            scrub:1,
-        }
-    })
-    
+/////// HEADER CONTENT ////////
 
+tl.from(".header-content", {
+    opacity: 0
+})
+
+tl.from(".header-text h1", {
+    y: 100,
+    opacity: 0,
+    ease: "power4.out",
+    duration: 0.5,
+})
+
+tl.from(".header-text p", {
+    y: 100,
+    opacity: 0,
+    ease: "power4.out",
+    duration: 0.5
+})
+
+tl.from(".header-text a", {
+    y: 100,
+    opacity: 0,
+    ease: "power4.out",
+    duration: 0.5
+})
+
+
+
+
+//////////////////  GSAP ANIMATION FOR ABOUT PAGE //////////////////
+
+gsap.from(".image1", {
+    x: -900,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".image1",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 40%",
+        scrub: 2,
+    }
+})
+
+gsap.from(".image2", {
+    y: 200,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".image2",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 40%",
+        scrub: 1,
+    }
+})
+
+gsap.from(".image3", {
+    x: 900,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".image3",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 40%",
+        scrub: 2,
+    }
+})
+
+
+
+//////////////////  GSAP ANIMATION FOR SERVICES PAGE //////////////////
+
+gsap.from(".box h2", {
+    y: -50,
+    opacity: 0,
+    stagger: 0.4,
+    scrollTrigger: {
+        trigger: ".box h2",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 65%",
+        scrub: 1,
+    }
+})
+
+gsap.from(".box p", {
+    x: -400,
+    opacity: 0,
+    stagger: 0.4,
+    scrollTrigger: {
+        trigger: ".box p",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 65%",
+        scrub: 1,
+    }
+})
+
+gsap.from(".see-more span", {
+    opacity: 0,
+    stagger: 0.4,
+    scrollTrigger: {
+        trigger: ".see-more span",
+        scroller: "body",
+        start: "top 90%",
+        end: "top 80%",
+        scrub: 1,
+    }
+})
+
+
+
+
+//////////////////  GSAP ANIMATION FOR QUOTE PAGE //////////////////
+
+gsap.from(".quote-wrapper", {
+    scale: 0.5,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".quote-wrapper",
+        scroller: "body",
+        start: "top 70%",
+        end: "top 40%",
+        scrub: 2,
+    }
+})
--- javascripts/script.js
+++ javascripts/script.js
@@ -4,41 +4,36 @@
 
 
 window.onload = function () {
-  window.scrollTo(0, 0);
 
+    window.scrollTo(0, 0);
 
-  const scroller = new LocomotiveScroll({
-    el: document.querySelector('[#main]'),
-    smooth: true
-  });
+    // Check if the URL contains a fragment identifier
+    // Check if the URL contains a fragment identifier
+    if (window.location.hash) {
+        // Smooth scroll to the element with the corresponding ID
+        var targetElement = document.querySelector(window.location.hash);
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+            // Replace the URL without adding a new entry to the history
+            history.replaceState(null, null, ' ');
+        }
+    }
 
+    // Add an event listener to update the URL when a navigation link is clicked
+    document.querySelectorAll('nav a').forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            var targetId = this.getAttribute('href').substring(1);
+            var targetElement = document.getElementById(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+                // Update the URL without adding a new entry to the history
+                history.replaceState(null, null, ' ');
+            }
+        });
+    });
 
 };
 
 
 
-// Check if the URL contains a fragment identifier
-// Check if the URL contains a fragment identifier
-if (window.location.hash) {
-    // Smooth scroll to the element with the corresponding ID
-    var targetElement = document.querySelector(window.location.hash);
-    if (targetElement) {
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-        // Replace the URL without adding a new entry to the history
-        history.replaceState(null, null, ' ');
-    }
-}
-
-// Add an event listener to update the URL when a navigation link is clicked
-document.querySelectorAll('nav a').forEach(function (link) {
-    link.addEventListener('click', function (event) {
-        event.preventDefault();
-        var targetId = this.getAttribute('href').substring(1);
-        var targetElement = document.getElementById(targetId);
-        if (targetElement) {
-            targetElement.scrollIntoView({ behavior: 'smooth' });
-            // Update the URL without adding a new entry to the history
-            history.replaceState(null, null, ' ');
-        }
-    });
-});
